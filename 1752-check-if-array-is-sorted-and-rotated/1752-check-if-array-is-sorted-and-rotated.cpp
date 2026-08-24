d359e132a22ea842276e30facbad0f623a4463fc
Karishma Kaushik <kaushikkarishma20@gmail.com>
2026-08-24 20:39:05 +0530

    Time: 0 ms (100%), Space: 11.3 MB (19.04%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {  //true false btane wala function
+        int n=nums.size(); //array size
+        int count=0; 
+        for(int i=0;i<n;i++){  //iteration
+            if(nums[i] > nums[(i+1)%n]){
+                count++;
+            }
+        }
+    return count<=1;  //A sorted-and-rotated array can have at most ONE place where the order decreases. If there are 2 or more such breaks, it cannot be obtained by rotating a sorted array.
+    }
+};
