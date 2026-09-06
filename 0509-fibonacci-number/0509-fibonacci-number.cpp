3185fb5e2f2df68e8c25736bc81b4eb9fe8dc489
Karishma Kaushik <kaushikkarishma20@gmail.com>
2026-09-06 12:51:31 +0530

    Time: 10 ms (45.16%), Space: 7.9 MB (38.37%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int fib(int n) {  //return an integer always
-        
-        if(n==0 || n==1){
+        if(n==0 || n==1){ //base cases 
             return n;
-        }
+        }  
         if(n>1){
-            return (fib(n-1)+fib(n-2));
+            return (fib(n-1)+fib(n-2)); //recursive cases
         }
         return 0;
     }
